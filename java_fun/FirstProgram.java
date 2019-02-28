7e2e1b8e36cd761e6bb1846d7f797d9465b0aed1
Albert Garcia <albertgarcia@Alberts-MacBook-Pro.local>
2019-02-28 11:52:15 -0600

    first commit

---
+++ java_fun/FirstProgram.java
@@ -0,0 +1,10 @@
+public class FirstProgram {
+    public static void main(String[] args){
+        String name = "Albert Garcia";
+        int age = 28;
+        String homeTown = "Amarillo, TX";
+        System.out.println("My name is " + name);
+        System.out.println("My age is " + age);
+        System.out.println("My home town is " + homeTown);
+    }
+}
