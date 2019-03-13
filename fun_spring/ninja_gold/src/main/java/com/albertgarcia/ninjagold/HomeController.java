4bc2a700b0b9b91643e5a1d65045ffd65f67b2e9
Albert Garcia <albert.garcia10180@gmail.com>
2019-03-13 15:10:00 -0500

    added ninja gold

---
+++ fun_spring/ninja_gold/src/main/java/com/albertgarcia/ninjagold/HomeController.java
@@ -0,0 +1,88 @@
+package com.albertgarcia.ninjagold;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+	private List<String> log = new ArrayList<>();
+	@GetMapping("/")
+	public String home(HttpSession session) {
+		session.setAttribute("log", this.log);
+		if(session.getAttribute("gold") == null) {
+			session.setAttribute("gold", 0);
+		}
+		return "index.jsp";
+	}
+	@PostMapping("/farm")
+	public String farm(HttpSession session) {
+		int gold = (int) session.getAttribute("gold");
+		gold += this.random("farm");
+		session.setAttribute("gold", gold);
+		return "redirect:/";
+	}
+	@PostMapping("/cave")
+	public String cave(HttpSession session) {
+		int gold = (int) session.getAttribute("gold");
+		gold += this.random("cave");
+		session.setAttribute("gold", gold);
+		return "redirect:/";
+	}
+	@PostMapping("/house")
+	public String house(HttpSession session) {
+		int gold = (int) session.getAttribute("gold");
+		gold += this.random("house");
+		session.setAttribute("gold", gold);
+		return "redirect:/";
+	}
+	@PostMapping("/casino")
+	public String casion(HttpSession session) {
+		int gold = (int) session.getAttribute("gold");
+		gold += this.random("casino");
+		session.setAttribute("gold", gold);
+		return "redirect:/";
+	}
+	public int random(String place) {
+		int r = 0;
+		Date date = new Date();
+		Random rand = new Random();
+		if(place.equals("farm")) {
+			r += (rand.nextInt(21 - 10) + 10);
+			this.log.add(0, "You Entered a " + place + " and earned " + r + " gold! (" + date + ")");
+		}
+		else if(place.equals("cave")) {
+			r += (rand.nextInt(11 - 5) + 5);
+			this.log.add(0, "You Entered a " + place + " and earned " + r + " gold! (" + date + ")");
+		}
+		else if(place.equals("house")) {
+			r += (rand.nextInt(5 - 2) + 2);
+			this.log.add(0, "You Entered a " + place + " and earned " + r + " gold! (" + date + ")");
+		}
+		else if(place.equals("casino")) {
+			int gamble = rand.nextInt(2);
+			if(gamble == 1) {
+				r += rand.nextInt(51);
+				this.log.add(0, "You Entered a " + place + " and earned " + r + " gold! (" + date + ")");
+			} else {
+				r -= rand.nextInt(51);
+				this.log.add(0, "You Entered a " + place + " and lost " + r + " gold... Ouch! " + date);
+			}
+		}
+		return r;
+	}
+	@GetMapping("/reset")
+	public String reset(HttpSession session) {
+		this.log.clear();
+		session.setAttribute("gold", 0);
+		return "redirect:/";
+		
+	}
+}
---
+++ fun_spring/ninja_gold/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,72 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<%@ page isErrorPage="true" %>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Ninja Gold</title>
+	    <link rel="stylesheet" type="text/css" href="css/style.css">
+		
+	</head>
+	<body>
+		<h1>Your gold <c:out value="${gold }"></c:out> </h1>
+		<div id="container">
+			<div id="" class="place">
+				<h2>Farm</h2>
+				<h4>(Earn 10 - 20 gold)</h4>
+				<form action="/farm" method="POST">
+					<button type="submit">Find Gold!</button>
+				</form>
+			</div>
+			<div id="" class="place">
+				<h2>Cave</h2>
+				<h4>(Earn 5 - 10 gold)</h4>
+				<form action="/cave" method="POST">
+					<button type="submit">Find Gold!</button>
+				</form>
+			</div>
+			<div id="" class="place">
+				<h2>House</h2>
+				<h4>(Earn 2 - 5 gold)</h4>
+				<form action="/house" method="POST">
+					<button type="submit">Find Gold!</button>
+				</form>
+			</div>
+	
+				<div id="" class="place">
+					<h2>Casino</h2>
+					<h4>(Earn/Lose 0 - 50 gold)</h4>
+					<c:if test="${gold >= 50 }">
+						<form action="/casino" method="POST">
+							<button type="submit">Find Gold!</button>
+						</form>
+					</c:if>
+				</div>
+				
+				<div id="activity">
+				
+					<c:if test="${log.size() > 0 }">
+					
+						<c:forEach var= "i" begin="${0 }" end="${log.size() - 1 }">
+						
+							<c:if test="${ log.get(i).contains('earn')}">
+								<p class="green"><c:out value="${log.get(i) }"></c:out></p>
+							</c:if>
+								
+							<c:if test="${log.get(i).contains('lost') }">
+								<p class="red"><c:out value="${log.get(i) }"></c:out></p>
+							</c:if>
+							
+						</c:forEach>
+						
+					</c:if>
+
+				</div>
+				
+			</div>
+			<a href="/reset"><button>Reset</button></a>
+
+	</body>
+</html>
